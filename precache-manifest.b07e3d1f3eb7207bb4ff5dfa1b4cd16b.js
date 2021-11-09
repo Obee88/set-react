3311518a71f44a3dd1f3095e6df362ad11c87b74
Davor Obilinovic <obee88@gmail.com>
2021-11-09 18:34:23 +0100

    Updates

---
+++ precache-manifest.b07e3d1f3eb7207bb4ff5dfa1b4cd16b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a260b7790ff0971b19be4bc54d7478a",
+    "url": "/set-react/index.html"
+  },
+  {
+    "revision": "d65d093cdf88f8b6c56b",
+    "url": "/set-react/static/css/main.efe229c5.chunk.css"
+  },
+  {
+    "revision": "eb927e1cff1afe90f31c",
+    "url": "/set-react/static/js/2.e9e7adb9.chunk.js"
+  },
+  {
+    "revision": "d65d093cdf88f8b6c56b",
+    "url": "/set-react/static/js/main.66614ca1.chunk.js"
+  },
+  {
+    "revision": "21cd5149bd09b3ce5e7b",
+    "url": "/set-react/static/js/runtime-main.0ac6c988.js"
+  }
+]);
